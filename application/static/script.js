a473679e039c7f5f734d55a3e0938399498fe73d
Pranav Rao <pranavrao145@gmail.com>
2020-08-17 22:16:02 -0400

    created projects pane, edited homepage, set layout for contact and about

--- application/static/script.js
+++ application/static/script.js
@@ -6,28 +6,3 @@ function makeactive(id) {
 	document.getElementById(id).classList.toggle("active");
 	active = id;
 }
-
-function wait(ms)
-{
-    var d = new Date();
-    var d2 = null;
-    do { d2 = new Date(); }
-    while(d2-d < ms);
-}
-
-function empty() {
-
-}
-
-function typeWrite(text, elemid) {
-  for (i = 0; i < text.length; i++) {
-    document.getElementById(elemid).innerHTML += text.charAt(i);
-    console.log(text.charAt(i));
-    wait(50)
-    setTimeout(empty, 50) // alternatively, still doesn't work!!
-  }
-} // why does this not work
-
-function main() {
-  typeWrite("Heyy", "init")
-}
