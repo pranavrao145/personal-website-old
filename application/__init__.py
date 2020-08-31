a8c76564f859c0381f084fd1ac719070a439386d
pranavrao145 <56097527+pranavrao145@users.noreply.github.com>
2020-08-30 22:35:09 -0400

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -10,7 +10,7 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.environ.get('EMAIL_USER')
 app.config["MAIL_PASSWORD"] = os.environ.get('EMAIL_PASS')
-app.config["MAIL_DEFAULT_SENDER"] = "noreply@demo.com"
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get('EMAIL_USER')
 
 mail = Mail(app)
 
